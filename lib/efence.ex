0b500218a5660eb3dd5c4dae72fa2a0457bb10f8
liuwei <liuwei@zenchn.com>
2018-01-10 14:14:42 +0800

    create project efence

---
+++ lib/efence.ex
@@ -0,0 +1,18 @@
+defmodule Efence do
+  @moduledoc """
+  Documentation for Efence.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Efence.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/efence_test.exs
@@ -0,0 +1,8 @@
+defmodule EfenceTest do
+  use ExUnit.Case
+  doctest Efence
+
+  test "greets the world" do
+    assert Efence.hello() == :world
+  end
+end
