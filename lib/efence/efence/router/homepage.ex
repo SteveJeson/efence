e775601a2e536660d6cca994af7447b6e63648c4
liuwei <liuwei@zenchn.com>
2018-01-16 13:26:31 +0800

    efence rest interface

--- lib/efence/efence/router/homepage.ex
+++ lib/efence/efence/router/homepage.ex
@@ -1,10 +1,37 @@
-defmodule Efence.Router.Homepage do
-  @moduledoc false
-  
+defmodule Efence.Router.User do
   use Maru.Router
 
-  get do
-    json(conn, %{ hello: :world })
+  params do
+    requires :code,    type: String #电子围栏编码
+    requires :deviceCodes, type: String #设备ID
+    requires :interval, type: Integer #运行时间间隔
+  end
+
+  post "startEfence" do
+    json(conn, %{ success: true,
+      statusCode: 1,
+      message: "启动电子围栏成功！",
+      data: conn.params})
+  end
+
+  post "stopEfence" do
+    json(conn, %{ success: true,
+      statusCode: 1,
+      message: "已成功停止电子围栏！",
+      data: conn.params})
+  end
+
+end
+
+defmodule Efence.Router.Homepage do
+  use Maru.Router
+
+  resources do
+    get do
+      json(conn, %{ hello: :world })
+    end
+
+    mount Efence.Router.User
   end
 end
 
